88bfea09ad24c46748a6e0b5c27c869a42cf625b
pepe_ <191390@utags.edu.mx>
2020-11-25 21:28:42 -0600

    Realizada la ruta de usuarios con la funcion post con BD

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,32 +1,41 @@
 const express = require('express');
+const Usuario = require('../models/usuario');
 const app = express();
 
 //la constante app es constante servidor la que se encarga de procesar todo, y con un . se invocan las funciones
 //request es lo que manda el cliente y respond es lo que le respondemos al cliente
 app.get('/usuario', function (req, res) {
   //send envia texto en html
-res.json({
-  ok:'200',
+  res.json({
+  ok: 200,
   mensaje:'Usuario consultado con exito'
-});
+  });
 });
 
 app.post('/usuario', function (req, res) {
-  let nombre = req.body.nombre;
   let body = req.body;
-  
-  if(nombre === undefined){
-    res.status(400).json({
-    ok: 400,
-    mensaje:'Favor de enviar el valor del nombre'
-    });
-  }else{
-    res.json({
-      ok:'200',
-      mensaje:'Tu usuario fue insertado con exito',
-      body:body
+  let usr = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: body.password
+  });
+
+  usr.save((err, usrDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        msg: 'Ocurrio un error',
+        err
       });
-  }
+    }
+
+    res.json({
+      ok: true,
+      msg: 'Usuario insertado con exito',
+      usrDB
+    });
+  });
+
 });
 
 app.put('/usuario/:id/:nombre', function (req, res) {
